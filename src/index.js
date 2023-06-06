448fb03254f270d5ca046329998688478ccb3b37
Christian-pprogrammer <mpanoc6@gmail.com>
2023-06-06 17:08:00 +0200

    fix: make navbar responsive

--- src/index.js
+++ src/index.js
@@ -26,17 +26,24 @@ const showLoader = () => loader.classList.remove("loader--hide");
 
 const hideLoader = () => loader.classList.add("loader--hide");
 
-setTimeout(
-  () =>
-    // the show/hide functions are passed as props
-    ReactDOM.render(
-      <Suspense fallback="...loading">
-        <App hideLoader={hideLoader} showLoader={showLoader} />
-      </Suspense>,
-      document.getElementById("root")
-    ),
-  1000
-);
+// setTimeout(
+//   () =>
+//     // the show/hide functions are passed as props
+//     ReactDOM.render(
+//       <Suspense fallback="...loading">
+//         <App hideLoader={hideLoader} showLoader={showLoader} />
+//       </Suspense>,
+//       document.getElementById("root")
+//     ),
+//   1000
+// );
+
+ReactDOM.render(
+  <Suspense fallback="...loading">
+    <App hideLoader={hideLoader} />
+  </Suspense>,
+  document.getElementById("root")
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
